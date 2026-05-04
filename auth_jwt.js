a1eef1c50800a1cbc6263bef70253982f351d657
Winn Pham <wpham213@gmail.com>
2026-05-03 20:45:23 -0600

    fix auth

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,14 +1,17 @@
-export function submitLogin(user) {
-  return fetch(`${process.env.REACT_APP_API_URL}/signin`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user)
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (data.token) {
-      localStorage.setItem("token", data.token);
+const passport = require('passport');
+
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    const user = { name: "cu_user" };
+
+    if (username === user.name && password === "cu_rulez") {
+      return done(null, user);
+    } else {
+      return done(null, false);
     }
-    return data;
-  });
-}
+  }
+));
+
+exports.isAuthenticated = passport.authenticate('basic', { session: false });
